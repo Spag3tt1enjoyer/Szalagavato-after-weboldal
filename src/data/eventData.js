7f69a53a6945b5d56b11fe7951ba5d9352c6804e
Spag3tt1enjoyer <trifonov.ignac@gmail.com>
2026-09-25 10:48:38 +0200

    Fix GitHub Pages image and music paths

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -49,21 +49,21 @@ export const nav = [
 export const artists = [
 
 
-  { id: 'siryx', name: 'DJ 1', genre: 'HARDSTYLE', image: '/images/cover.jpg',
+  { id: 'siryx', name: 'DJ 1', genre: 'HARDSTYLE', image: './images/cover.jpg',
     description: 'valami leírás',
 
-    instagram: 'https://instagram.com/', soundcloud: 'https://soundcloud.com/', previewTrackId: 'break-it-down' },
+    instagram: 'https://instagram.com/', soundcloud: 'https://soundcloud.com/', previewTrackId: 'acid-cry' },
 
-  { id: 'dj-example', name: 'DJ EXAMPLE', genre: 'DRUM & BASS', image: '/images/dj-example.jpg',
+  { id: 'dj-example', name: 'DJ EXAMPLE', genre: 'DRUM & BASS', image: './images/dj-example.jpg',
     description: 'Gyors törések és mély szub, ami a mellkasodban rezeg.',
     instagram: 'https://instagram.com/', soundcloud: '', previewTrackId: 'example' },
 
-  { id: 'artist-3', name: 'ARTIST NAME', genre: 'HARD TECHNO', image: '/images/artist-3.jpg',
+  { id: 'artist-3', name: 'ARTIST NAME', genre: 'HARD TECHNO', image: './images/artist-3.jpg',
     description: 'Könyörtelen tempó, sötét hangzás. Cseréld le a leírást.', instagram: '', soundcloud: '' , previewTrackId: 'break-it-down' },
 
-  { id: 'artist-4', name: 'ARTIST NAME', genre: 'INDUSTRIAL TECHNO', image: '/images/artist-4.jpg',
+  { id: 'artist-4', name: 'ARTIST NAME', genre: 'INDUSTRIAL TECHNO', image: './images/artist-4.jpg',
     description: 'Fém, gőz és nyomás a legmélyebb szinten. Cseréld le a leírást.', instagram: '', soundcloud: '' , previewTrackId: 'break-it-down' },
 
-  { id: 'artist-5', name: 'ARTIST NAME', genre: 'INDUSTRIAL TECHNO', image: '/images/artist-4.jpg',
+  { id: 'artist-5', name: 'ARTIST NAME', genre: 'INDUSTRIAL TECHNO', image: './images/artist-4.jpg',
     description: 'Fém, gőz és nyomás a legmélyebb szinten. Cseréld le a leírást.', instagram: '', soundcloud: '' , previewTrackId: 'break-it-down' },
 ]
